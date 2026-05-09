53cce94dc0d13c6f5ae2cb4426a3c7cf95215fc9
louissxe <louissyhputra@gmail.com>
2026-05-09 22:03:49 +0700

    Update Loader-LuxvS.lua

--- Loader-LuxvS.lua
+++ Loader-LuxvS.lua
@@ -8,29 +8,27 @@
 ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝   ╚═╝      ╚═╝   
 Developed by Louissxe - https://louissxe.store
 ]]--
+
+
+
+
 repeat task.wait() until game:IsLoaded()
-
 local LuxvsHub = {}
-
 LuxvsHub.Universal = "https://raw.githubusercontent.com/louissxe/Lua/refs/heads/main/keyless/Universal.lua"
-
 LuxvsHub.Games = {
     [89469502395769] = {
         name = "Kick a lucky block",
         url = "https://raw.githubusercontent.com/louissxe/Lua/refs/heads/main/keyless/kickalucky.lua"
     },
-
     [70845479499574] = {
         name = "Bite by night",
         url = "https://raw.githubusercontent.com/louissxe/Lua/refs/heads/main/keyless/bitebynight.lua"
     },
-
     [130274245431977] = {
         name = "Climb and Plunge",
         url = "https://raw.githubusercontent.com/louissxe/Lua/refs/heads/main/keyless/ClimbAndPlunge.lua"
     }
 }
-
 function LuxvsHub.Notify(title, text, duration)
     pcall(function()
         game:GetService("StarterGui"):SetCore("SendNotification", {
@@ -40,12 +38,10 @@ function LuxvsHub.Notify(title, text, duration)
         })
     end)
 end
-
 function LuxvsHub.Load(url)
     local success, result = pcall(function()
         return loadstring(game:HttpGet(url))()
     end)
-
     if success then
         LuxvsHub.Notify(
             "LuxvsHub",
@@ -58,22 +54,16 @@ function LuxvsHub.Load(url)
             tostring(result),
             8
         )
-
         warn("[LuxvsHub]:", result)
     end
-
     return result
 end
-
 local gameData = LuxvsHub.Games[game.PlaceId]
-
 local scriptURL = gameData and gameData.url or LuxvsHub.Universal
 local gameName = gameData and gameData.name or "Universal"
-
 LuxvsHub.Notify(
     "LuxvsHub",
     "Loading "..gameName.."...",
     5
 )
-
 LuxvsHub.Load(scriptURL)
